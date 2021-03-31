4ad93f4137c3629f4bae5950a3985a8b1d4921b4
Prashant Kumar <prashant.kumar@taazaa.com>
2021-03-31 13:26:40 +0530

    base exception class added

---
+++ todo/todo.domain/Exceptions/BaseException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todo.domain.Exceptions
+{
+    public class BaseException : ApplicationException
+    {
+        public BaseException()
+        {
+
+        }
+
+        public BaseException(string message) : base(message)
+        {
+
+        }
+
+        public BaseException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
